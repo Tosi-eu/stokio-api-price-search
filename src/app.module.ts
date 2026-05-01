544aac852c24e79c8954b3f9afb2a415650c69e4
Tosi-eu <galdiniguilherme@usp.br>
2026-05-01 19:52:30 -0300

    feat: using temporal to administrate scheduled jobs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { AppConfigModule } from './config/app-config.module';
 import { SearchModule } from './search/search.module';
 import { AppController } from './controllers/app.controller';
-import { PriceBackfillTriggerCron } from './cron/price-backfill-trigger.cron';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), AppConfigModule, SearchModule],
+  imports: [AppConfigModule, SearchModule],
   controllers: [AppController],
-  providers: [PriceBackfillTriggerCron],
 })
 export class AppModule {}
